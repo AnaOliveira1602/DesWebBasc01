15fc78b0352ced2295f95b826eec0f89d19dfb34
AnaOliveira1602 <126676071+AnaOliveira1602@users.noreply.github.com>
2023-05-26 11:32:13 -0300

    Add files via upload

---
+++ Aula2605/script.js
@@ -0,0 +1,20 @@
+document.write("");
+
+let medo;
+
+
+alert("To cum sono");
+medo = prompt("Informe seu medo: ");
+confirm("Concorda em se fuder?");
+
+document.write("Medo acionado: " + medo);
+
+document.querySelector(".container");
+
+let minha_div = document.querySelector("div");
+
+minha_div.innerHTML = "<p>Alguma coisa</p>";
+
+minha_div.innerHTML = "<img src='./imagem/sukuna.jpg' width='250px' height='150px' alt='Sukuna'>";
+
+/*outra forma de separar as "" é isando / antes delas.*/
